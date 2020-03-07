c473161af82f44bccd337e5749de4998d47060e0
Sungwoo Park <codest99@gmail.com>
2020-03-07 22:18:10 +0900

    chore: Tag 컴포넌트 추가

---
+++ src/frameworks/web/components/atoms/Tag/Tag.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import theme from 'theme';
+
+export const BaseTag = styled.span`
+  font-family: 'Muli', 'Noto Sans KR', sans-serif;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 24px;
+  color: white;
+  padding: 4px 8px 4px 8px;
+  border-radius: 8px;
+`;
+
+export const AcceptTag = styled(BaseTag)`
+  background-color: ${theme.color.alert.Success};
+`;
+
+export const PendingTag = styled(BaseTag)`
+  background-color: #9B9B9B;
+`;
+
+export const UnacceptTag = styled(BaseTag)`
+background-color: ${theme.color.alert.Failure};
+`;
