4f29c8c4a9f9f3fb64d8cba132d1a220ae524103
Sungwoo Park <codest99@gmail.com>
2020-03-10 15:53:22 +0900

    chore: Tag 스토리북 수정

--- src/frameworks/web/components/atoms/Tag/Tag.stories.tsx
+++ src/frameworks/web/components/atoms/Tag/Tag.stories.tsx
@@ -14,18 +14,6 @@ export const DefaultTag = () => {
   return <Tag color={pickColor}>{label}</Tag>;
 };
 
-export const ProgressTag = () => (
-  <>
-    <Tag color={theme.color.alert.Success}>승인</Tag>
-    <Tag color={theme.color.secondary.Moon}>심사중</Tag>
-    <Tag color={theme.color.alert.Failure}>미승인</Tag>
-  </>
-);
-
 DefaultTag.story = {
   name: 'Default Tag',
-};
-
-ProgressTag.story = {
-  name: 'Progress Tag',
 };
