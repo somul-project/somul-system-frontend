fdbeac2fdf3c37d9d6b8d84daacbd64ec55d286e
Sungwoo Park <codest99@gmail.com>
2020-03-12 18:58:37 +0900

    fix: select 컴포넌트 hover시 width값이 변하는 오류 수정

--- src/frameworks/web/components/atoms/Select/Select.tsx
+++ src/frameworks/web/components/atoms/Select/Select.tsx
@@ -1,22 +1,24 @@
 import styled from 'styled-components';
 import theme from 'theme';
 import React from 'react';
-// eslint-disable-next-line no-unused-vars
-import { ISelect, ISelectElement, EdgeType } from 'interfaces/frameworks/web/components/atoms/Select/ISelect';
+import {
+  // eslint-disable-next-line no-unused-vars
+  ISelect, ISelectElement, EdgeType, ISelectState,
+} from 'interfaces/frameworks/web/components/atoms/Select/ISelect';
 
 const SelectElement = styled.button`
   font-family: 'Muli', 'Noto Sans KR', sans-serif;
   font-size: 16px;
 
+  border: solid 2px ${(props: ISelectElement) => (props.active ? theme.color.primary.Azure : theme.color.secondary.Ash)};
   background-color: ${(props: ISelectElement) => (props.active ? theme.color.primary.Azure : theme.color.secondary.Ash)};
   color: ${(props: ISelectElement) => (props.active ? theme.color.primary.White : theme.color.secondary.Coal)};
 
   width: 100%;
   height: 56px;
-  border: none;
   outline: none;
-  
   transition: all 0.2s;
+  
   margin-left: ${(props: ISelectElement) => (props.edge === 'left' ? '0px' : '2px')};
   margin-right: ${(props: ISelectElement) => (props.edge === 'right' ? '0px' : '2px')};
   border-top-left-radius: ${(props: ISelectElement) => (props.edge === 'left' ? '10px' : '4px')};
@@ -37,22 +39,27 @@ const SelectContainer = styled.div`
     display: flex;
     flex-grow: 1;
     justify-content: center;
-    flex-basis: 0;
   }
 `;
 
-export default class Select extends React.PureComponent<ISelect> {
-  state = {
-    selectNumber: -1,
+export default class Select extends React.PureComponent<ISelect, ISelectState> {
+  constructor(props: Readonly<ISelect>) {
+    super(props);
+    this.state = {
+      selectNumber: -1,
+    };
   }
 
   handleClick(i: number) {
+    const { onElementClick } = this.props;
     const { selectNumber } = this.state;
     if (i === selectNumber) {
+      onElementClick(-1);
       this.setState({
         selectNumber: -1,
       });
     } else {
+      onElementClick(i);
       this.setState({
         selectNumber: i,
       });
