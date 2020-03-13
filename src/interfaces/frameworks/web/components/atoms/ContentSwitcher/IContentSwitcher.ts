66145b641dbcfb7c3b38db9f1d57011cc41b6cc9
Dora Lee <dsa28s@naver.com>
2020-03-13 20:13:15 +0900

    feat: ContentSwitcher 컴포넌트 추가

---
+++ src/interfaces/frameworks/web/components/atoms/ContentSwitcher/IContentSwitcher.ts
@@ -0,0 +1,14 @@
+export interface IBaseButtonContentButton {
+    active: boolean
+}
+
+export type EdgeType = 'left' | 'right' | 'none';
+
+export interface IContentButton extends IBaseButtonContentButton {
+    edge: EdgeType
+}
+
+export interface IContentSwitcher {
+    index: number
+    labels: string[]
+}
