a64ce942a191b3e7452c4ae82ed1790b7446ffe2
Sungwoo Park <codest99@gmail.com>
2020-03-19 00:36:45 +0900

    refactor: 변수명 의도 명확하게 변경, 아토믹 디자인에 맞게 수정

--- src/frameworks/web/components/atoms/ContentsBox/ContentsBox.tsx
+++ src/frameworks/web/components/atoms/ContentsBox/ContentsBox.tsx
@@ -1,15 +1,11 @@
 import styled from 'styled-components';
-
-interface IContentsBox {
-  width: string
-  height: string
-  bgDark: boolean
-}
+// eslint-disable-next-line no-unused-vars
+import { IContentsBox } from 'interfaces/frameworks/web/components/atoms/ContentsBox/IContentsBox';
 
 const ContentsBox = styled.div`
-  width: ${(props: IContentsBox) => props.width ?? '200px'};
-  height: ${(props: IContentsBox) => props.height ?? '200px'};
-  box-shadow: 0 5px 30px 0 rgba(155, 155, 155, ${(props: IContentsBox) => (props.bgDark ? '0.1' : '0.2')});
+  width: ${(props: IContentsBox) => props.width ?? 200}px;
+  height: ${(props: IContentsBox) => props.height ?? 200}px;
+  box-shadow: 0 5px 30px 0 rgba(155, 155, 155, ${(props: IContentsBox) => (props.isDarkBackground ? '0.1' : '0.2')});
   background-color: white;
   border-radius: 25px;
 `;
