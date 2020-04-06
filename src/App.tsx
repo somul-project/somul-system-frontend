bac4c53a0418c0ad627cb734ab1778f8e2e51960
Sungwoo Park <codest99@gmail.com>
2020-04-06 13:07:15 +0900

    fix: IE11에서 열리지 않는 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Landing from 'frameworks/web/components/pages/Landing/Landing';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 function App() {
   return (
