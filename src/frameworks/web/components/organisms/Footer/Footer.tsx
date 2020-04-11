8292c5a5eefd55def5f0829021e313ed2d809809
Sungwoo Park <codest99@gmail.com>
2020-04-11 20:42:48 +0900

    fix: margin값 수정, 이미지 위치 수정

--- src/frameworks/web/components/organisms/Footer/Footer.tsx
+++ src/frameworks/web/components/organisms/Footer/Footer.tsx
@@ -6,9 +6,13 @@ import {
   Row, Col, Visible, Hidden,
 } from 'react-grid-system';
 
+const MaxContainer = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+`;
+
 const FooterContainer = styled.div`
-  height: 144px;
-  padding: 0 85px;
+  padding: 40px 54px;
   background: ${theme.color.primary.Black};
 `;
 
@@ -26,7 +30,7 @@ const CodeClubLogo = styled.img`
 
 const FooterCenterContainer = styled.div`
   width: 100%;
-  height: 100%;
+  height: ;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -36,50 +40,54 @@ const FooterCenterContainer = styled.div`
 export default class Footer extends React.PureComponent {
   render() {
     return (
-      <FooterContainer>
-        <Visible lg xl>
-          <Row align="center" justify="between" style={{ height: '100%', margin: '0' }}>
-            <Col md={4}>
-              <Label type="H5" color={theme.color.secondary.Moon}>소프트웨어에 물들다</Label>
-              <Label type="P2" color={theme.color.secondary.Moon}>Colored in Software</Label>
-            </Col>
-            <Col lg={6}>
-              <div style={{ float: 'right', marginLeft: '230px' }}>
-                <a href="https://www.facebook.com/groups/may.somul/">
-                  <FooterSNSLogo src="facebook-footer.svg" alt="footer facebook icon" />
-                </a>
-                <a href="https://github.com/somul-project">
-                  <FooterSNSLogo src="github-footer.svg" alt="footer github icon" />
-                </a>
-                <a href="https://codeclubkorea.org">
-                  <CodeClubLogo src="codeclub.svg" alt="footer code-club icon" />
-                </a>
-              </div>
-              <Label type="P2" color={theme.color.secondary.Moon} style={{ float: 'right' }}>
-                Copyright © 2020 Colored by Software, All rights reserved.
-              </Label>
-            </Col>
-          </Row>
-        </Visible>
-        <Hidden lg xl>
-          <FooterCenterContainer>
-            <div>
-              <a href="https://www.facebook.com/groups/may.somul/">
-                <FooterSNSLogo src="facebook-footer.svg" alt="footer facebook icon" />
-              </a>
-              <a href="https://github.com/somul-project">
-                <FooterSNSLogo src="github-footer.svg" alt="footer github icon" />
-              </a>
-              <a href="https://codeclubkorea.org">
-                <CodeClubLogo src="codeclub.svg" alt="footer code-club icon" />
-              </a>
-            </div>
-            <Label type="MobileP2" color={theme.color.secondary.Moon} style={{ marginTop: '8px', textAlign: 'center' }}>
-              Copyright © 2020 Colored by Software, All rights reserved.
-            </Label>
-          </FooterCenterContainer>
-        </Hidden>
-      </FooterContainer>
+      <div style={{ background: theme.color.primary.Black }}>
+        <MaxContainer>
+          <FooterContainer>
+            <Visible lg xl>
+              <Row align="center" justify="between" style={{ height: '100%', margin: '0' }}>
+                <Col md={4}>
+                  <Label type="H5" color={theme.color.secondary.Moon}>소프트웨어에 물들다</Label>
+                  <Label type="P2" color={theme.color.secondary.Moon}>Colored in Software</Label>
+                </Col>
+                <Col lg={6}>
+                  <div style={{ float: 'right', marginLeft: '230px' }}>
+                    <a href="https://www.facebook.com/groups/may.somul/">
+                      <FooterSNSLogo src="facebook-footer.svg" alt="footer facebook icon" />
+                    </a>
+                    <a href="https://github.com/somul-project">
+                      <FooterSNSLogo src="github-footer.svg" alt="footer github icon" />
+                    </a>
+                    <a href="https://codeclubkorea.org">
+                      <CodeClubLogo src="codeclub.svg" alt="footer code-club icon" />
+                    </a>
+                  </div>
+                  <Label type="P2" color={theme.color.secondary.Moon} style={{ float: 'right' }}>
+                    Copyright © 2020 Colored by Software, All rights reserved.
+                  </Label>
+                </Col>
+              </Row>
+            </Visible>
+            <Hidden lg xl>
+              <FooterCenterContainer>
+                <div>
+                  <a href="https://www.facebook.com/groups/may.somul/">
+                    <FooterSNSLogo src="facebook-footer.svg" alt="footer facebook icon" />
+                  </a>
+                  <a href="https://github.com/somul-project">
+                    <FooterSNSLogo src="github-footer.svg" alt="footer github icon" />
+                  </a>
+                  <a href="https://codeclubkorea.org">
+                    <CodeClubLogo src="codeclub.svg" alt="footer code-club icon" />
+                  </a>
+                </div>
+                <Label type="MobileP2" color={theme.color.secondary.Moon} style={{ marginTop: '8px', textAlign: 'center' }}>
+                  Copyright © 2020 Colored by Software, All rights reserved.
+                </Label>
+              </FooterCenterContainer>
+            </Hidden>
+          </FooterContainer>
+        </MaxContainer>
+      </div>
     );
   }
 }
