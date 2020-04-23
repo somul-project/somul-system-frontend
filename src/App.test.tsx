c5023cf181be6a3c12649167276c922f558ef949
Sungwoo Park <codest99@gmail.com>
2020-04-23 09:13:20 +0900

    chore: UserService 테스트코드 작성

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders landing page link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/ABOUT SOMUL/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/utils/user.ts
+++ src/utils/user.ts
@@ -10,15 +10,15 @@ export class UserService {
    * @returns true or 에러메세지
    */
   public static signUpValidationCheck = (data: ISignUpData) => {
-    const emailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-    const passwordRegExp = /^[a-zA-Z0-9]*$/i;
+    const emailRegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+    const passwordRegExp = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}$/i;
     const phoneRegExp = /^[0-9]*$/i;
 
-    if (data.isPrivacyChecked === false) {
-      return '개인정보처리방침 동의가 필요합니다.';
-    }
     if (data.email === '' || data.name === '' || data.password === '' || data.phone === '' || data.rePassword === '') {
       return '모든 칸을 입력해야 합니다.';
+    }
+    if (data.isPrivacyChecked === false) {
+      return '개인정보처리방침 동의가 필요합니다.';
     }
     if (!emailRegExp.test(data.email)) {
       return '이메일 형식이 올바르지 않습니다.';
@@ -26,7 +26,7 @@ export class UserService {
     if (data.phone.length !== 11 || !phoneRegExp.test(data.phone)) {
       return '휴대폰 번호 형식이 올바르지 않습니다.';
     }
-    if (!passwordRegExp.test(data.password) || data.password.length < 8) {
+    if (!passwordRegExp.test(data.password)) {
       return '비밀번호 형식이 올바르지 않습니다.';
     }
     if (data.password !== data.rePassword) {
