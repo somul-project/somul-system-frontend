de3373dafb111e76aec084dc98cf278119a01d5c
Gihyeon Yang <gihyeon@molocoads.com>
2020-05-10 03:35:19 +0900

    refactor: SignUp 관련 컴포넌트 수정

--- src/frameworks/web/components/organisms/SignUp/SignUpCard.tsx
+++ src/frameworks/web/components/organisms/SignUp/SignUpCard.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable no-undef */
 import React from 'react';
 import styled from 'styled-components';
-import theme from 'theme';
 import { Link } from 'react-router-dom';
+
+import theme from 'theme';
 import Label from 'frameworks/web/components/atoms/Label/Label';
 import ContentsBox from 'frameworks/web/components/atoms/ContentsBox/ContentsBox';
 import SignButton from 'frameworks/web/components/atoms/SignButton/SignButton';
@@ -20,35 +20,33 @@ const SignButtonContainer = styled.div`
   margin: 0 auto;
 `;
 
-export default class SignUpCard extends React.PureComponent {
-  googleSignUp = () => {
+export default function SignUpCard(): React.ReactElement {
+  const googleSignUp = () => {
     const win = window.open(`${SERVER_URL}/auth/google`, '_blank');
     win!.focus();
   };
 
-  githubSignUp = () => {
+  const githubSignUp = () => {
     const win = window.open(`${SERVER_URL}/auth/github`, '_blank');
     win!.focus();
   };
 
-  render() {
-    return (
-      <SignInContainer isDarkBackground>
-        <Label type="H4" color={theme.color.primary.Azure} style={{ textAlign: 'center' }}>
-          SIGN UP
-        </Label>
-        <SignButtonContainer>
-          <div style={{ marginBottom: '24px' }}>
-            <SignButton siteType="google" buttonType="signup" onClick={this.googleSignUp} />
-          </div>
-          <div style={{ marginBottom: '24px' }}>
-            <SignButton siteType="github" buttonType="signup" onClick={this.githubSignUp} />
-          </div>
-          <Link to="/signup">
-            <SignButton siteType="email" buttonType="signup" onClick={() => undefined} />
-          </Link>
-        </SignButtonContainer>
-      </SignInContainer>
-    );
-  }
+  return (
+    <SignInContainer isDarkBackground>
+      <Label type="H4" color={theme.color.primary.Azure} style={{ textAlign: 'center' }}>
+        SIGN UP
+      </Label>
+      <SignButtonContainer>
+        <div style={{ marginBottom: '24px' }}>
+          <SignButton siteType="google" buttonType="signup" onClick={googleSignUp} />
+        </div>
+        <div style={{ marginBottom: '24px' }}>
+          <SignButton siteType="github" buttonType="signup" onClick={githubSignUp} />
+        </div>
+        <Link to="/signup">
+          <SignButton siteType="email" buttonType="signup" onClick={() => undefined} />
+        </Link>
+      </SignButtonContainer>
+    </SignInContainer>
+  );
 }
--- src/frameworks/web/components/organisms/SignUp/SignUpComplete.tsx
+++ src/frameworks/web/components/organisms/SignUp/SignUpComplete.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import theme from 'theme';
+import { useLocation, useHistory } from 'react-router';
+
 import SingleCard from 'frameworks/web/components/molecules/SingleCard/SingleCard';
 import Label from 'frameworks/web/components/atoms/Label/Label';
-// eslint-disable-next-line no-unused-vars
-import { RouteComponentProps } from 'react-router-dom';
 import UserService from 'utils/user';
 import { ERROR_MESSAGE } from 'utils/constants';
 
-const resend = async () => {
-  const result = await UserService.requestResendEmail();
-  // eslint-disable-next-line no-undef, no-alert
-  alert(result === '0' ? '재전송되었습니다.' : ERROR_MESSAGE[result] ?? ERROR_MESSAGE['500']);
-};
+export default function SignUpComplete(): React.ReactElement {
+  const history = useHistory();
+  const location = useLocation();
 
-const goHome = () => {
-  // eslint-disable-next-line no-undef
-  window.location.href = '/';
-};
+  const email = new URLSearchParams(location.search).get('email');
 
-const SignUpComplete = (props: RouteComponentProps) => {
-  const { location } = props;
+  const goHome = () => {
+    history.push('/');
+  };
+
+  const resend = async () => {
+    const result = await UserService.requestResendEmail();
+    alert(result === '0' ? '재전송되었습니다.' : ERROR_MESSAGE[result] ?? ERROR_MESSAGE['500']);
+  };
+
   return (
     <SingleCard
       title="인증메일이 전송되었습니다!"
       buttonLabel={['재전송', '확인']}
       buttonOnClick={[resend, goHome]}
     >
-      <Label type="H3">{new URLSearchParams(location.search).get('email')}</Label>
+      <Label type="H3">{email}</Label>
       <Label type="P1" color={theme.color.secondary.Moon} style={{ marginTop: '24px' }}>
         위 주소로 인증메일이 전송되었습니다.
       </Label>
@@ -35,6 +37,4 @@ const SignUpComplete = (props: RouteComponentProps) => {
       </Label>
     </SingleCard>
   );
-};
-
-export default SignUpComplete;
+}
