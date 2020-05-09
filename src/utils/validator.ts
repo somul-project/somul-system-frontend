f97e56c55361aadbd8a688022bd453974d600f7f
Gihyeon Yang <gihyeon@molocoads.com>
2020-05-10 03:08:25 +0900

    feat: validator util 추가

---
+++ src/utils/validator.ts
@@ -0,0 +1,6 @@
+export const isRequired = (value: any) => !!value;
+
+export const emailFormat = (email: string) => {
+  const regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regExp.test(email);
+};
