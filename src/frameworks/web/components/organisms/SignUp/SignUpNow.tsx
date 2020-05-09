f8ce2169cd4858e56af9a1069378d518f78175b1
Dora Lee <dsa28s@naver.com>
2020-05-10 05:15:58 +0900

    refactor(sign-up): SignUpNow 컴포넌트를 Functional Component 로 변경하고, hook 을 사용하도록 리팩토링

--- src/frameworks/web/components/organisms/SignUp/SignUpNow.tsx
+++ src/frameworks/web/components/organisms/SignUp/SignUpNow.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unused-state */
 /* eslint-disable no-undef */
 /* eslint-disable no-alert */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import theme from 'theme';
 import DividedCard from 'frameworks/web/components/molecules/DividedCard/DividedCard';
@@ -10,10 +10,10 @@ import TextField from 'frameworks/web/components/atoms/TextField/TextField';
 import TextArea from 'frameworks/web/components/atoms/TextArea/TextArea';
 import Button from 'frameworks/web/components/atoms/Button/Button';
 import CheckBox from 'frameworks/web/components/atoms/CheckBox/CheckBox';
-// eslint-disable-next-line no-unused-vars
-import { ISignUpData } from 'interfaces/utils/user/IUserService';
-import UserService from 'utils/user';
-import { ERROR_MESSAGE } from 'utils/constants';
+
+import SignUpIllust from 'assets/illust/signup-illustration.png';
+import { IRegisterUserValidateState } from 'interfaces/utils/IValidator';
+import { isEmail } from 'utils/validator';
 
 const TextLabelContainer = styled.div`
   width: 65px;
@@ -47,149 +47,247 @@ const PrivacyContainer = styled.div`
   clear: both;
 `;
 
-export default class SignUpNow extends React.PureComponent<{}, ISignUpData> {
-  constructor(props: Readonly<{}>) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      phone: '',
-      password: '',
-      rePassword: '',
-      isPrivacyChecked: false,
-    };
-  }
+export default function SignUpNow(): React.ReactElement {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [rePassword, setRePassword] = useState('');
+  const [isAgreeLicense, setAgreeLicense] = useState(false);
 
-  signupTry = async () => {
-    const signupPayload = this.state;
-    const valCheck = UserService.signUpValidationCheck(signupPayload);
-    if (valCheck !== true) {
-      alert(valCheck);
-      return;
-    }
-    const signUpResult = await UserService.sendSignUpData(signupPayload);
-    if (signUpResult === '0') {
-      window.location.href = `/signup/complete?email=${signupPayload.email}`;
-      return;
-    }
-    alert(ERROR_MESSAGE[signUpResult] ?? ERROR_MESSAGE['500']);
+  const [isRegisterButtonEnabled, setRegisterButtonEnabled] = useState(false);
+
+  // -1 : 초기 상태, 0 : 유저가 입력하려고 포커스까진 했음, 1 : 특정 오류, 2 : 비어있는 칸 오류, 3 : 정상
+  const [validate, setValidate] = useState<IRegisterUserValidateState>({
+    nameValidStatus: -1,
+    emailValidStatus: -1,
+    phoneNumberValidStatus: -1,
+    passwordValidStatus: -1,
+  });
+
+  const checkAllDataValidated = () => {
+    // @ts-ignore
+    const isFieldPass = Object.keys(validate).filter((key) => validate[key] !== 3).length === 0;
+    setRegisterButtonEnabled(isFieldPass && isAgreeLicense);
   };
 
-  render() {
-    const { isPrivacyChecked } = this.state;
-    return (
-      <DividedCard title="SIGN UP NOW">
-        {{
-          left: (
-            <div>
-              <img
-                src="illust/signup-illustration.png"
-                alt="회원가입 이미지"
-                style={{ width: '380px', height: '580px', margin: '73px 0 18px 0' }}
-              />
-              <Label type="P1" color={theme.color.primary.White} style={{ paddingBottom: '28px' }}>
-                SOMUL 가입을 환영합니다 :)
+  const validateInputData = (type: 'name' | 'email' | 'phoneNumber' | 'password') => {
+    if (type === 'name') {
+      if (!name || name.length <= 0) {
+        setValidate({ ...validate, nameValidStatus: 2 });
+      } else {
+        setValidate({ ...validate, nameValidStatus: 3 });
+      }
+    } else if (type === 'email') {
+      if (!email || email.length <= 0) {
+        setValidate({ ...validate, emailValidStatus: 2 });
+      } else if (!isEmail(email)) {
+        setValidate({ ...validate, emailValidStatus: 1 });
+      } else {
+        setValidate({ ...validate, emailValidStatus: 3 });
+      }
+    } else if (type === 'phoneNumber') {
+      if (!phoneNumber || phoneNumber.length <= 0) {
+        setValidate({ ...validate, phoneNumberValidStatus: 2 });
+      } else {
+        setValidate({ ...validate, phoneNumberValidStatus: 3 });
+      }
+    } else if (type === 'password') {
+      if (!password || password.length <= 0) {
+        setValidate({ ...validate, passwordValidStatus: 2 });
+      } else if (rePassword && password !== rePassword) {
+        setValidate({ ...validate, passwordValidStatus: 1 });
+      } else if (password === rePassword) {
+        setValidate({ ...validate, passwordValidStatus: 3 });
+      }
+    }
+  };
+
+  // 이건 나중에 오류 문구가 들어가야 할 때 쓰라우...!
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
+  const getErrorMessage = (type: 'name' | 'email' | 'phoneNumber' | 'password') => {
+    if (type === 'name') {
+      if (validate.nameValidStatus === 2) {
+        return '이름 칸은 비어있을 수 없습니다.';
+      }
+    } else if (type === 'email') {
+      if (validate.emailValidStatus === 1) {
+        return '잘못된 이메일이 입력되었습니다.';
+      }
+      if (validate.emailValidStatus === 2) {
+        return '이메일 칸은 비어있을 수 없습니다.';
+      }
+    } else if (type === 'phoneNumber') {
+      if (validate.phoneNumberValidStatus === 2) {
+        return '휴대폰 칸은 비어있을 수 없습니다.';
+      }
+    } else if (type === 'password') {
+      if (validate.passwordValidStatus === 1) {
+        return '비밀번호가 일치하지 않습니다.';
+      }
+      if (validate.passwordValidStatus === 2) {
+        return '비밀번호 칸은 비어있을 수 없습니다.';
+      }
+    }
+
+    return undefined;
+  };
+
+  useEffect(() => {
+    checkAllDataValidated();
+  }, [validate, isAgreeLicense]);
+
+  const signUpTry = async () => {
+    // const signupPayload = { name, email, phoneNumber, password, rePassword, isAgreeLicense };
+  };
+
+  return (
+    <DividedCard title="SIGN UP NOW">
+      {{
+        left: (
+          <div>
+            <img
+              src={SignUpIllust}
+              alt="회원가입 이미지"
+              style={{ width: '380px', height: '580px', margin: '73px 0 18px 0' }}
+            />
+            <Label type="P1" color={theme.color.primary.White} style={{ paddingBottom: '28px' }}>
+              SOMUL 가입을 환영합니다 :)
+            </Label>
+          </div>
+        ),
+        right: (
+          <div style={{ margin: '0 95px' }}>
+            <TextLabelContainer>
+              <Label
+                type="H5"
+                style={{ marginTop: '14px', marginBottom: '54px' }}
+                color={theme.color.secondary.Nickel}
+              >
+                이름
               </Label>
-            </div>
-          ),
-          right: (
-            <div style={{ margin: '0 95px' }}>
-              <TextLabelContainer>
-                <Label
-                  type="H5"
-                  style={{ marginTop: '14px', marginBottom: '54px' }}
-                  color={theme.color.secondary.Nickel}
-                >
-                  이름
-                </Label>
-                <Label type="H5" color={theme.color.secondary.Nickel}>
-                  이메일
-                </Label>
-                <Label
-                  type="H5"
-                  style={{ marginTop: '82px', marginBottom: '56px' }}
-                  color={theme.color.secondary.Nickel}
-                >
-                  휴대폰
-                </Label>
-                <Label type="H5" color={theme.color.secondary.Nickel}>
-                  비밀번호
-                </Label>
-              </TextLabelContainer>
-              <TextFieldContainer>
-                <TextField
-                  defaultLabel="이름을 입력하세요"
-                  onValueChange={(value) => this.setState({ name: value })}
-                  style={{ width: 'auto', marginBottom: '24px' }}
+              <Label type="H5" color={theme.color.secondary.Nickel}>
+                이메일
+              </Label>
+              <Label
+                type="H5"
+                style={{ marginTop: '82px', marginBottom: '56px' }}
+                color={theme.color.secondary.Nickel}
+              >
+                휴대폰
+              </Label>
+              <Label type="H5" color={theme.color.secondary.Nickel}>
+                비밀번호
+              </Label>
+            </TextLabelContainer>
+            <TextFieldContainer>
+              <TextField
+                defaultLabel="이름을 입력하세요"
+                onValueChange={(value) => setName(value)}
+                onFocusChanged={(value) => {
+                  if (!value) validateInputData('name');
+                }}
+                style={{ width: 'auto' }}
+                isError={validate.nameValidStatus === 1 || validate.nameValidStatus === 2}
+              />
+              <div style={{ height: '24px' }} />
+              <TextField
+                defaultLabel="이메일을 입력하세요"
+                onValueChange={(value) => setEmail(value)}
+                onFocusChanged={(value) => {
+                  if (!value) validateInputData('email');
+                }}
+                isError={validate.emailValidStatus === 1 || validate.emailValidStatus === 2}
+                style={{ width: 'auto' }}
+              />
+              <WarningTextContainer>
+                <img
+                  src="warning.svg"
+                  alt="경고 아이콘"
+                  style={{ width: '20px', height: '18px' }}
                 />
-                <TextField
-                  defaultLabel="이메일을 입력하세요"
-                  onValueChange={(value) => this.setState({ email: value })}
-                  style={{ width: 'auto' }}
-                />
-                <WarningTextContainer>
-                  <img
-                    src="warning.svg"
-                    alt="경고 아이콘"
-                    style={{ width: '20px', height: '18px' }}
-                  />
-                  <div style={{ display: 'flex' }}>
-                    <Label type="P2" color={theme.color.alert.Warning}>
-                      인증메일
-                    </Label>
-                    <Label type="P2" color={theme.color.secondary.Moon}>
-                      을 발송할 예정이니, 유효한 메일을 입력해주세요.
-                    </Label>
-                  </div>
-                </WarningTextContainer>
-                <TextField
-                  defaultLabel="휴대폰 번호를 입력하세요 (01012345678)"
-                  onValueChange={(value) => this.setState({ phone: value })}
-                  style={{ width: 'auto', clear: 'right' }}
-                />
-                <PasswordContainer>
-                  <div style={{ width: '210px' }}>
-                    <TextField
-                      defaultLabel="비밀번호를 입력하세요"
-                      onValueChange={(value) => this.setState({ password: value })}
-                      style={{ width: 'auto' }}
-                      type="password"
-                    />
-                  </div>
-                  <div style={{ width: '210px' }}>
-                    <TextField
-                      defaultLabel="비밀번호를 재입력하세요"
-                      onValueChange={(value) => this.setState({ rePassword: value })}
-                      style={{ width: 'auto' }}
-                      type="password"
-                    />
-                  </div>
-                </PasswordContainer>
-                <Label type="P2" color={theme.color.secondary.Moon} style={{ float: 'right' }}>
-                  비밀번호는 8글자 이상의 숫자와 문자 조합이어야 합니다.
-                </Label>
-              </TextFieldContainer>
-              <PrivacyContainer>
-                <TextArea
-                  defaultLabel={`1. 개인정보 처리방침이란?
-반갑습니다!
-소프트웨어에 물들다(이하 ‘소물')는 참여자의 ‘동의를 기반으로 개인정보를 수집·이용 및 제공’하고 있으며, ‘이용자의 권리 (개인정보 자기결정권)를 적극적으로 보장’합니다. 소물은 정보통신서비스제공자가 준수하여야 하는 대한민국의 관계 법령 및 개인정보보호 규정, 가이드라인을 준수하고 있습니다. “개인정보 처리방침”이란 참여자의 소중한 개인정보를 보호함으로써 참여자가 안심하고 소물에 참여할 수 있도록 소물이 준수해야 할 지침을 의미합니다. 본 개인정보 처리방침은 소물 강연 참여 신청 시스템에 적용됩니다`}
-                  onValueChange={() => undefined}
-                  readOnly
-                />
-                <div style={{ float: 'right', marginTop: '16px' }}>
-                  <CheckBox
-                    label="위 약관을 자세히 읽었으며, 개인정보처리방침에 동의합니다."
-                    checked={isPrivacyChecked}
-                    onChange={() => this.setState({ isPrivacyChecked: !isPrivacyChecked })}
+                <div style={{ display: 'flex' }}>
+                  <Label type="P2" color={theme.color.alert.Warning}>
+                    인증메일
+                  </Label>
+                  <Label type="P2" color={theme.color.secondary.Moon}>
+                    을 발송할 예정이니, 유효한 메일을 입력해주세요.
+                  </Label>
+                </div>
+              </WarningTextContainer>
+              <TextField
+                defaultLabel="휴대폰 번호를 입력하세요 (01012345678)"
+                onValueChange={(value) => setPhoneNumber(value)}
+                onFocusChanged={(value) => {
+                  if (!value) validateInputData('phoneNumber');
+                }}
+                isError={
+                  validate.phoneNumberValidStatus === 1 || validate.phoneNumberValidStatus === 2
+                }
+                style={{ width: 'auto', clear: 'right' }}
+              />
+              <PasswordContainer>
+                <div style={{ width: '210px' }}>
+                  <TextField
+                    defaultLabel="비밀번호를 입력하세요"
+                    onValueChange={(value) => setPassword(value)}
+                    onFocusChanged={(value) => {
+                      if (!value) validateInputData('password');
+                    }}
+                    isError={
+                      validate.passwordValidStatus === 1 || validate.passwordValidStatus === 2
+                    }
+                    style={{ width: 'auto' }}
+                    type="password"
                   />
                 </div>
-              </PrivacyContainer>
-              <Button type="wide" label="회원가입" isPrimary={false} onClick={this.signupTry} />
-            </div>
-          ),
-        }}
-      </DividedCard>
-    );
-  }
+                <div style={{ width: '210px' }}>
+                  <TextField
+                    defaultLabel="비밀번호를 재입력하세요"
+                    onValueChange={(value) => setRePassword(value)}
+                    onFocusChanged={(value) => {
+                      if (!value) validateInputData('password');
+                    }}
+                    isError={
+                      validate.passwordValidStatus === 1 || validate.passwordValidStatus === 2
+                    }
+                    style={{ width: 'auto' }}
+                    type="password"
+                  />
+                </div>
+              </PasswordContainer>
+              <Label type="P2" color={theme.color.secondary.Moon} style={{ float: 'right' }}>
+                비밀번호는 8글자 이상의 숫자와 문자 조합이어야 합니다.
+              </Label>
+            </TextFieldContainer>
+            <PrivacyContainer>
+              <TextArea
+                defaultLabel={`1. 개인정보 처리방침이란?
+반갑습니다!
+소프트웨어에 물들다(이하 ‘소물')는 참여자의 ‘동의를 기반으로 개인정보를 수집·이용 및 제공’하고 있으며, ‘이용자의 권리 (개인정보 자기결정권)를 적극적으로 보장’합니다. 소물은 정보통신서비스제공자가 준수하여야 하는 대한민국의 관계 법령 및 개인정보보호 규정, 가이드라인을 준수하고 있습니다. “개인정보 처리방침”이란 참여자의 소중한 개인정보를 보호함으로써 참여자가 안심하고 소물에 참여할 수 있도록 소물이 준수해야 할 지침을 의미합니다. 본 개인정보 처리방침은 소물 강연 참여 신청 시스템에 적용됩니다`}
+                onValueChange={() => undefined}
+                readOnly
+              />
+              <div style={{ float: 'right', marginTop: '16px' }}>
+                <CheckBox
+                  label="위 약관을 자세히 읽었으며, 개인정보처리방침에 동의합니다."
+                  checked={isAgreeLicense}
+                  onChange={() => {
+                    setAgreeLicense(!isAgreeLicense);
+                  }}
+                />
+              </div>
+            </PrivacyContainer>
+            <Button
+              type="wide"
+              label="회원가입"
+              isPrimary={false}
+              isEnabled={isRegisterButtonEnabled}
+              onClick={signUpTry}
+            />
+          </div>
+        ),
+      }}
+    </DividedCard>
+  );
 }
