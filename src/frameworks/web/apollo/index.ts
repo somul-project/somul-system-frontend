b7d49aa4747df5680b2e9bc02865ee3befac8414
Dora Lee <dsa28s@naver.com>
2020-05-10 01:01:33 +0900

    feat: GraphQL / Rest API 를 사용하기 위한 Apollo Client 추가

---
+++ src/frameworks/web/apollo/index.ts
@@ -0,0 +1,25 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import { persistCache } from 'apollo-cache-persist';
+import { RestLink } from 'apollo-link-rest';
+import { ApolloLink } from 'apollo-link';
+import { SERVER_URL } from '../../../utils/constants';
+
+const cache = new InMemoryCache();
+const graphqlLink = createHttpLink({ uri: `${SERVER_URL}/graphql` });
+const restLink = new RestLink({ uri: `${SERVER_URL}` });
+
+const apolloClient = new ApolloClient({
+  // @ts-ignore ApolloClient Typescript 정의 파일에 문제가 있어서 무조건 오류가 납니다..
+  link: ApolloLink.from([graphqlLink, restLink]),
+  cache: new InMemoryCache(),
+});
+
+persistCache({
+  cache,
+  // @ts-ignore
+  storage: window.localStorage,
+});
+
+export default apolloClient;
