446c75a2dbc1ff089ef2f873cc42b86c6bd5c186
Dora Lee <dsa28s@naver.com>
2020-05-10 05:20:39 +0900

    refactor(sign-up): SignUpNow 컴포넌트를 Functional Component 로 변경하고, hook 을 사용하도록 리팩토링

--- src/frameworks/web/components/atoms/SignButton/SignButton.tsx
+++ src/frameworks/web/components/atoms/SignButton/SignButton.tsx
@@ -1,11 +1,15 @@
+import React from 'react';
 import styled from 'styled-components';
 import theme from 'theme';
-import React from 'react';
-// eslint-disable-next-line no-unused-vars
+
 import {
   ISignButton,
   ISignButtonElement,
 } from 'interfaces/frameworks/web/components/atoms/SignButton/ISignButton';
+
+import GoogleLogo from 'assets/logo/google.svg';
+import GithubLogo from 'assets/logo/github.svg';
+import EmailLogo from 'assets/logo/email.svg';
 
 const SignButtonContainer = styled.div`
   background-color: ${(props: ISignButtonElement) => {
@@ -50,40 +54,27 @@ export default function SignButton({
   siteType,
   onClick,
 }: ISignButton): React.ReactElement {
+  let logo;
   let siteString: string;
   if (siteType === 'google') {
     siteString = 'Google';
+    logo = GoogleLogo;
   } else if (siteType === 'github') {
     siteString = 'Github';
+    logo = GithubLogo;
   } else {
     siteString = '이메일';
+    logo = EmailLogo;
   }
-export default class SignButton extends React.PureComponent<ISignButton> {
-  render() {
-    const { buttonType, siteType, onClick } = this.props;
 
-    let logo;
-    let siteString: string;
-    if (siteType === 'google') {
-      siteString = 'Google';
-      logo = GoogleLogo;
-    } else if (siteType === 'github') {
-      siteString = 'Github';
-      logo = GithubLogo;
-    } else {
-      siteString = '이메일';
-      logo = EmailLogo;
-    }
-
-    return (
-      <SignButtonContainer onClick={() => onClick()} siteType={siteType}>
-        <SignButtonImg src={logo} siteType={siteType} />
-        <SignButtonLabel>
-          {siteString}
-          {siteType === 'email' ? '로' : ' 계정으로'}{' '}
-          {buttonType === 'signin' ? '로그인' : '회원가입'}
-        </SignButtonLabel>
-      </SignButtonContainer>
-    );
-  }
+  return (
+    <SignButtonContainer onClick={() => onClick()} siteType={siteType}>
+      <SignButtonImg src={logo} siteType={siteType} />
+      <SignButtonLabel>
+        {siteString}
+        {siteType === 'email' ? '로' : ' 계정으로'}{' '}
+        {buttonType === 'signin' ? '로그인' : '회원가입'}
+      </SignButtonLabel>
+    </SignButtonContainer>
+  );
 }
