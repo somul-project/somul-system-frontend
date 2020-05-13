9a4a832e2cebf856cd2a4b2476d7b9dadf887fb0
Sungwoo Park <codest99@gmail.com>
2020-05-13 23:39:28 +0900

    feat: 강연 신청하지 않았을 경우 Status 페이지 추가 (#58)

---
+++ src/frameworks/web/components/pages/StatusPage/StatusPage.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import theme from 'theme';
+import Label from 'frameworks/web/components/atoms/Label/Label';
+import Button from 'frameworks/web/components/atoms/Button/Button';
+import ContentsBox from 'frameworks/web/components/atoms/ContentsBox/ContentsBox';
+import { IStatusPage } from 'interfaces/frameworks/web/components/pages/StatusPage/IStatusPage';
+
+const StatusContainer = styled.div`
+  padding: 160px 85px 120px 85px;
+  background-color: ${theme.color.secondary.Ash};
+  text-align: center;
+`;
+
+const StatusContentContainer = styled(ContentsBox)`
+  margin-top: 80px;
+  width: 100%;
+`;
+
+const NoStatusContainer = styled.div`
+  padding: 120px 0;
+`;
+
+export default function StatusPage({ isApply = false }: IStatusPage): React.ReactElement {
+  const history = useHistory();
+  const linkToApply = () => {
+    history.push('/apply/speaker');
+  };
+  return (
+    <StatusContainer>
+      <Label type="H3" color={theme.color.primary.Azure}>
+        나의 신청현황
+      </Label>
+      <StatusContentContainer isDarkBackground>
+        {!isApply && (
+          <NoStatusContainer>
+            <Label type="P1" color={theme.color.secondary.Moon} style={{ paddingBottom: '45px' }}>
+              신청하신 강연이 없습니다.
+            </Label>
+            <Button label="신청하기" onClick={linkToApply} />
+          </NoStatusContainer>
+        )}
+      </StatusContentContainer>
+    </StatusContainer>
+  );
+}
