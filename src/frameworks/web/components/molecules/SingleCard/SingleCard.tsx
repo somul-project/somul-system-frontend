f7a6d0bf09c25df9f8317e75062f5535c0091a91
Gihyeon Yang <dexterastin@gmail.com>
2020-05-13 22:22:09 +0900

    Refactor: Molecule component를 Functional Component로 변경 (#52)

--- src/frameworks/web/components/molecules/SingleCard/SingleCard.tsx
+++ src/frameworks/web/components/molecules/SingleCard/SingleCard.tsx
@@ -1,11 +1,10 @@
+import React from 'react';
 import styled from 'styled-components';
-// eslint-disable-next-line no-unused-vars
-import React, { ReactNode } from 'react';
 import theme from 'theme';
+
 import ContentsBox from 'frameworks/web/components/atoms/ContentsBox/ContentsBox';
 import Label from 'frameworks/web/components/atoms/Label/Label';
 import Button from 'frameworks/web/components/atoms/Button/Button';
-// eslint-disable-next-line no-unused-vars
 import { ISingleCard } from 'interfaces/frameworks/web/components/molecules/SingleCard/ISingleCard';
 
 const CardContainer = styled(ContentsBox)`
@@ -30,27 +29,29 @@ const EachButtonContainer = styled.div`
   margin: 0 15px 0 15px;
 `;
 
-export default class SingleCard extends React.PureComponent<ISingleCard> {
-  render() {
-    const { title, children, buttonLabel, buttonOnClick } = this.props;
-    const buttonElements: ReactNode[] = [];
-
-    buttonLabel.forEach((l, i) => {
-      buttonElements.push(
-        // eslint-disable-next-line react/no-array-index-key
-        <EachButtonContainer key={i}>
-          <Button label={l} isPrimary={i === buttonLabel.length - 1} onClick={buttonOnClick[i]} />
-        </EachButtonContainer>,
-      );
-    });
-    return (
-      <CardContainer isDarkBackground={false}>
-        <Label type="H4" color={theme.color.primary.Azure}>
-          {title}
-        </Label>
-        <ContextContainer>{children}</ContextContainer>
-        <ButtonContainer>{buttonElements}</ButtonContainer>
-      </CardContainer>
-    );
-  }
+export default function SingleCard({
+  title,
+  children,
+  buttonLabel,
+  buttonOnClick,
+}: ISingleCard): React.ReactElement {
+  return (
+    <CardContainer isDarkBackground={false}>
+      <Label type="H4" color={theme.color.primary.Azure}>
+        {title}
+      </Label>
+      <ContextContainer>{children}</ContextContainer>
+      <ButtonContainer>
+        {buttonLabel.map((label, i) => (
+          <EachButtonContainer key={label}>
+            <Button
+              label={label}
+              isPrimary={i === buttonLabel.length - 1}
+              onClick={buttonOnClick[i]}
+            />
+          </EachButtonContainer>
+        ))}
+      </ButtonContainer>
+    </CardContainer>
+  );
 }
