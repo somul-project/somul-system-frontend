a5313be55ca0d9ea3cb323b0eb3ed6af2b69c4e4
Gihyeon Yang <gihyeon@molocoads.com>
2020-05-17 05:39:42 +0900

    feat: YouTube URL 검사 URL 객체 사용하도록 변경

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -23,9 +23,35 @@ export const isPhoneNumber = (pNumber: string): boolean => {
   return phoneNumberRegex.test(pNumber.trim());
 };
 
-export const isValidYoutubeLink = (link: string): boolean => {
-  const youtubeLinkRegex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|v\/)?)([\w-]+)(\S+)?$/;
-  return youtubeLinkRegex.test(String(link.trim()));
+export const isURL = (url: string): boolean => {
+  if (url.trim().length <= 0) return false;
+
+  const urlRegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
+  return urlRegExp.test(url);
+};
+
+export const isYouTubeURL = (url: string): boolean => {
+  if (!isURL(url)) return false;
+
+  try {
+    const HASH_LENGTH = 11;
+    const HOST_NAMES = { SHORT: 'youtu.be', LONG: 'youtube.com' };
+
+    const { protocol, hostname, pathname, searchParams } = new URL(url);
+
+    if (protocol !== 'https') return false;
+    if (hostname.includes(HOST_NAMES.LONG)) {
+      const v = searchParams.get('v') || '';
+      return pathname === '/watch' && v.length === 11;
+    }
+    if (hostname.includes(HOST_NAMES.SHORT)) {
+      return pathname.length === HASH_LENGTH;
+    }
+
+    return false;
+  } catch {
+    return false;
+  }
 };
 
 export const isValidPassword = (password: string): boolean => {
