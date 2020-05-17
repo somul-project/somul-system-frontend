c73db36e9d52004a5b5a867d0c21abeb61d52ac6
Sungwoo Park <codest99@gmail.com>
2020-05-18 02:14:44 +0900

    chore: StatusContent에 표시되는 데이터를 props로 빼냄

--- src/frameworks/web/components/organisms/StatusPage/StatusContent.tsx
+++ src/frameworks/web/components/organisms/StatusPage/StatusContent.tsx
@@ -27,6 +27,11 @@ const ContentContainer = styled.div`
   padding: 48px 95px 72px 95px;
 `;
 
+const TitleAndVideoContainer = styled.div`
+  min-height: 92px;
+  width: 100%;
+`;
+
 const TitleContainer = styled.div`
   max-width: 540px;
   float: left;
@@ -51,7 +56,15 @@ const SpeakerContainer = styled.div`
   margin-bottom: 16px;
 `;
 
-export default function StatusContent({ statusNum = 0 }: IStatusContent): React.ReactElement {
+export default function StatusContent({
+  statusNum,
+  applyDate,
+  title,
+  video,
+  speaker,
+  bio,
+  description,
+}: IStatusContent): React.ReactElement {
   const statusString = ['심사중', '반려', '심사중', '승인완료'];
   const statusColor = [
     theme.color.secondary.Moon,
@@ -59,16 +72,16 @@ export default function StatusContent({ statusNum = 0 }: IStatusContent): React.
     theme.color.secondary.Moon,
     theme.color.alert.Success,
   ];
-  const videoArray = new Array(3).fill('https://youtube.com');
   return (
     <div>
       <ContentHeaderContainer>
+        {/* 하단 DateContainer는 정렬을 맞추기 위한 dummy Container입니다 */}
         <DateContainer>
           <Label type="H5" color="white" style={{ marginRight: '24px' }}>
             신청일
           </Label>
           <Label type="P1" color="white">
-            04.23
+            {applyDate}
           </Label>
         </DateContainer>
         <Label type="H4" color={theme.color.secondary.Moon}>
@@ -79,21 +92,21 @@ export default function StatusContent({ statusNum = 0 }: IStatusContent): React.
           <Label type="H5" style={{ marginRight: '24px' }}>
             신청일
           </Label>
-          <Label type="P1">04.23</Label>
+          <Label type="P1">{applyDate}</Label>
         </DateContainer>
       </ContentHeaderContainer>
       <SingleLine />
       <ContentContainer>
-        <div>
+        <TitleAndVideoContainer>
           <TitleContainer>
             <Label type="H4" color={theme.color.primary.Azure} style={{ textAlign: 'left' }}>
-              클라우드와 AI로 바뀌는 세상
+              {title}
             </Label>
           </TitleContainer>
           <VideoButtonContainer>
-            {videoArray.map((href, i) => {
+            {video.map((href, i) => {
               return (
-                <div key={href} style={{ marginRight: i === videoArray.length - 1 ? '0' : '32px' }}>
+                <div key={href} style={{ marginRight: i === video.length - 1 ? '0' : '32px' }}>
                   <VideoButton href={href} />
                   <Label
                     type="P2"
@@ -106,54 +119,32 @@ export default function StatusContent({ statusNum = 0 }: IStatusContent): React.
               );
             })}
           </VideoButtonContainer>
-          <Row justify="between" gutterWidth={30} style={{ marginTop: '48px' }}>
-            <Col xs={5}>
-              <IntroduceContainer>
-                <SpeakerContainer>
-                  <div style={{ display: 'flex' }}>
-                    <Label type="H5" color={theme.color.secondary.Moon}>
-                      강연자
-                    </Label>
-                    <Label type="P1" style={{ marginLeft: '16px' }}>
-                      김수민
-                    </Label>
-                  </div>
-                </SpeakerContainer>
-                <Label type="P2" color={theme.color.secondary.Moon} style={{ textAlign: 'left' }}>
-                  따뜻한 평화스러운 얼음 심장의 청춘을 사막이다. 않는 품고 반짝이는 때문이다.
-                  위하여, 싸인 생의 가진 그들의 사막이다. 얼음이 열락의 목숨이 들어 굳세게 위하여,
-                  내려온 봄날의 대한 이것이다. 아름답고 고행을 인간의 오직 이상을 싹이 청춘은 청춘
-                  있다. 그와 피부가 예가 소담스러운 뜨거운지라, 사막이다. 주며, 싶이 이 모래뿐일
-                  사막이다. 행복스럽고 무엇을 인간에 같이, 예수는 목숨이 있는가? 그러므로 그것은
-                  심장의 어디. 따뜻한 평화스러운 얼음 심장의 청춘을 사막이다. 않는 품고 반짝이는
-                  때문이다. 위하여, 싸인 생의 가진 그. (300자 Full)
-                </Label>
-              </IntroduceContainer>
-            </Col>
-            <Col xs={7} style={{ textAlign: 'left' }}>
-              <Label
-                type="H5"
-                color={theme.color.secondary.Moon}
-                style={{ margin: '24px 0 16px 0' }}
-              >
-                강연 내용
+        </TitleAndVideoContainer>
+        <Row justify="between" gutterWidth={30} style={{ marginTop: '48px' }}>
+          <Col xs={5}>
+            <IntroduceContainer>
+              <SpeakerContainer>
+                <div style={{ display: 'flex' }}>
+                  <Label type="H5" color={theme.color.secondary.Moon}>
+                    강연자
+                  </Label>
+                  <Label type="P1" style={{ marginLeft: '16px' }}>
+                    {speaker}
+                  </Label>
+                </div>
+              </SpeakerContainer>
+              <Label type="P2" color={theme.color.secondary.Moon} style={{ textAlign: 'left' }}>
+                {bio}
               </Label>
-              <Label type="P2">
-                따뜻한 평화스러운 얼음 심장의 청춘을 사막이다. 않는 품고 반짝이는 때문이다. 위하여,
-                싸인 생의 가진 그들의 사막이다. 얼음이 열락의 목숨이 들어 굳세게 위하여, 내려온
-                봄날의 대한 이것이다. 아름답고 고행을 인간의 오직 이상을 싹이 청춘은 청춘 있다. 그와
-                피부가 예가 소담스러운 뜨거운지라, 사막이다. 주며, 싶이 이 모래뿐일 사막이다.
-                행복스럽고 무엇을 인간에 같이, 예수는 목숨이 있는가? 그러므로 그것은 심장의 어디
-                무엇을 아니다. 따뜻한 평화스러운 얼음 심장의 청춘을 사막이다. 않는 품고 반짝이는
-                때문이다. 따뜻한 평화스러운 얼음 심장의 청춘을 사막이다. 않는 품고 반짝이는
-                때문이다. 위하여, 싸인 생의 가진 그들의 사막이다. 얼음이 열락의 목숨이 들어 굳세게
-                위하여, 내려온 봄날의 대한 이것이다. 아름답고 고행을 인간의 오직 이상을 싹이 청춘은
-                청춘 있다. 그와 피부가 예가 소담스러운 뜨거운지라, 사막이다. 주며, 싶이 이 모래뿐일
-                사막이다. 행복스럽고 무엇을 인간에 같이. (500자 Full)
-              </Label>
-            </Col>
-          </Row>
-        </div>
+            </IntroduceContainer>
+          </Col>
+          <Col xs={7} style={{ textAlign: 'left' }}>
+            <Label type="H5" color={theme.color.secondary.Moon} style={{ margin: '24px 0 16px 0' }}>
+              강연 내용
+            </Label>
+            <Label type="P2">{description}</Label>
+          </Col>
+        </Row>
       </ContentContainer>
     </div>
   );
