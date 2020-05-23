3811d7c9d0c9b9bc7e192befa2fee965c5449de9
Dora Lee <dsa28s@naver.com>
2020-05-24 02:54:19 +0900

    feat: [WIP] 내 신청 현황을 불러올 수 있는 기능 추가

--- src/frameworks/web/components/pages/StatusPage/StatusPage.tsx
+++ src/frameworks/web/components/pages/StatusPage/StatusPage.tsx
@@ -6,10 +6,20 @@ import Label from 'frameworks/web/components/atoms/Label/Label';
 import Button from 'frameworks/web/components/atoms/Button/Button';
 import ContentsBox from 'frameworks/web/components/atoms/ContentsBox/ContentsBox';
 import StatusContent from 'frameworks/web/components/organisms/StatusPage/StatusContent';
-import { IStatusPage } from 'interfaces/frameworks/web/components/pages/StatusPage/IStatusPage';
+import { Query } from 'react-apollo';
+import apolloClient from 'frameworks/web/apollo';
+
+import * as ROUTES from 'utils/routes';
+import { GET_USER } from 'service/graphql/query/User';
+import useCurrentSession from 'frameworks/web/hooks/CurrentSessionHook';
+import Loading from 'frameworks/web/components/atoms/Loading/Loading';
+import { IUserData } from 'interfaces/service/graphql/query/IUser';
+import { StatusType } from 'interfaces/frameworks/web/components/organisms/StatusPage/IStatusContent';
 
 const StatusContainer = styled.div`
-  padding: 160px 85px 120px 85px;
+  padding-top: 160px;
+  padding-bottom: 120px;
+  // padding: 160px 85px 120px 85px;
   background-color: ${theme.color.secondary.Ash};
   text-align: center;
   height: auto;
@@ -17,68 +27,84 @@ const StatusContainer = styled.div`
 `;
 
 const StatusContentContainer = styled(ContentsBox)`
-  margin-top: 80px;
-  width: 100%;
+  margin: 80px auto 0;
+  width: 1110px;
 `;
 
 const NoStatusContainer = styled.div`
   padding: 120px 0;
 `;
 
-export default function StatusPage({ isApply = true }: IStatusPage): React.ReactElement {
+export default function StatusPage(): React.ReactElement {
   const history = useHistory();
+  const [isCurrentSessionLoaded, currentSession] = useCurrentSession();
+
   const linkToApply = () => {
-    history.push('/apply/speaker');
+    history.push(ROUTES.APPLY_SPEAKER);
   };
 
-  // TODO: 아래 내용들을 GraphQL과 연동하여 사용자의 데이터를 가져와야 합니다.
-  const applyDate = '04.23';
-  const title =
-    '클라우드와 AI로 바뀌는 세상 클라우드와 AI로 바뀌는 세상 클라우드와 AI로 바뀌는 세상 (50자 Full)';
-  const video = new Array(3).fill('https://youtube.com');
-  const speaker = '김수민';
-  const bio = `따뜻한 평화스러운 얼음 심장의 청춘을 사막이다. 않는 품고 반짝이는 때문이다. 위하여, 싸인 생의 가진 그들의 사막이다. 얼음이 열락의 목숨이 들어 굳세게
-  위하여, 내려온 봄날의 대한 이것이다. 아름답고 고행을 인간의 오직 이상을 싹이 청춘은 청춘있다. 그와 피부가 예가 소담스러운 뜨거운지라, 사막이다. 주며, 싶이 이 모래뿐일 사막이다.
-  행복스럽고 무엇을 인간에 같이, 예수는 목숨이 있는가? 그러므로 그것은 심장의 어디. 따뜻한 평화스러운 얼음 심장의 청춘을 사막이다. 않는 품고 반짝이는 때문이다.
-  위하여, 싸인 생의 가진 그. (300자 Full)`;
-  const description = `따뜻한 평화스러운 얼음 심장의 청춘을 사막이다. 않는 품고 반짝이는 때문이다. 위하여,
-  싸인 생의 가진 그들의 사막이다. 얼음이 열락의 목숨이 들어 굳세게 위하여, 내려온
-  봄날의 대한 이것이다. 아름답고 고행을 인간의 오직 이상을 싹이 청춘은 청춘 있다. 그와
-  피부가 예가 소담스러운 뜨거운지라, 사막이다. 주며, 싶이 이 모래뿐일 사막이다.
-  행복스럽고 무엇을 인간에 같이, 예수는 목숨이 있는가? 그러므로 그것은 심장의 어디
-  무엇을 아니다. 따뜻한 평화스러운 얼음 심장의 청춘을 사막이다. 않는 품고 반짝이는
-  때문이다. 따뜻한 평화스러운 얼음 심장의 청춘을 사막이다. 않는 품고 반짝이는
-  때문이다. 위하여, 싸인 생의 가진 그들의 사막이다. 얼음이 열락의 목숨이 들어 굳세게
-  위하여, 내려온 봄날의 대한 이것이다. 아름답고 고행을 인간의 오직 이상을 싹이 청춘은
-  청춘 있다. 그와 피부가 예가 소담스러운 뜨거운지라, 사막이다. 주며, 싶이 이 모래뿐일
-  사막이다. 행복스럽고 무엇을 인간에 같이. (500자 Full)`;
-
   return (
-    <StatusContainer>
-      <Label type="H3" color={theme.color.primary.Azure}>
-        나의 신청현황
-      </Label>
-      <StatusContentContainer isDarkBackground>
-        {!isApply && (
-          <NoStatusContainer>
-            <Label type="P1" color={theme.color.secondary.Moon} style={{ paddingBottom: '45px' }}>
-              신청하신 강연이 없습니다.
-            </Label>
-            <Button label="신청하기" onClick={linkToApply} />
-          </NoStatusContainer>
-        )}
-        {isApply && (
-          <StatusContent
-            statusNum={0}
-            applyDate={applyDate}
-            title={title}
-            video={video}
-            speaker={speaker}
-            bio={bio}
-            description={description}
-          />
-        )}
-      </StatusContentContainer>
-    </StatusContainer>
+    <>
+      <StatusContainer>
+        <Label type="H3" color={theme.color.primary.Azure}>
+          나의 신청현황
+        </Label>
+        <StatusContentContainer isDarkBackground>
+          {isCurrentSessionLoaded && (
+            // @ts-ignore
+            <Query<IUserData, { email: string }>
+              query={GET_USER}
+              variables={{ email: currentSession?.email! }}
+              client={apolloClient}
+            >
+              {/* @ts-ignore */}
+              {({ loading, error, data }) => {
+                if (loading) {
+                  return <Loading />;
+                }
+
+                if (error) {
+                  return (
+                    <NoStatusContainer>
+                      <Label type="P1" color={theme.color.secondary.Moon}>
+                        신청한 강연 정보를 불러오는 데에 오류가 발생했습니다.
+                      </Label>
+                    </NoStatusContainer>
+                  );
+                }
+
+                if (data && data.user.sessions.length === 0) {
+                  return (
+                    <NoStatusContainer>
+                      <Label
+                        type="P1"
+                        color={theme.color.secondary.Moon}
+                        style={{ paddingBottom: '45px' }}
+                      >
+                        신청하신 강연이 없습니다.
+                      </Label>
+                      <Button label="신청하기" onClick={linkToApply} />
+                    </NoStatusContainer>
+                  );
+                }
+
+                return data!.user!.sessions!.map((session) => (
+                  <StatusContent
+                    statusNum={parseInt(session.admin_approved, 10) as StatusType}
+                    applyDate={session.createdAt}
+                    title={session.session_name}
+                    video={session.document.split(',')}
+                    speaker={data!.user!.name}
+                    bio={session.introduce}
+                    description="서버에 객체가 없어........"
+                  />
+                ));
+              }}
+            </Query>
+          )}
+        </StatusContentContainer>
+      </StatusContainer>
+      {!isCurrentSessionLoaded && <Loading />}
+    </>
   );
 }
