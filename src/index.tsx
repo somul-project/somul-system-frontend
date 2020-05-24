23aeb6b98f63453bdd6b2b589b70e0b88986c3a7
Dora Lee <dsa28s@naver.com>
2020-05-24 22:08:34 +0900

    chore: Sentry / GA 를 추가함

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import * as Sentry from '@sentry/browser';
+import ReactGA from 'react-ga';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import theme from './theme';
+import { REACT_APP_SENTRY_DSN, REACT_APP_GA_ID } from './utils/constants';
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({ dsn: REACT_APP_SENTRY_DSN });
+  ReactGA.initialize(REACT_APP_GA_ID as string);
+}
 
 // 글로벌 스타일을 적용하기 위함
 const GlobalStyle = createGlobalStyle`
